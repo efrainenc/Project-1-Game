91b8821c7fcfac8fa64714a92508b43895f67a2e
unknown <efraine387@gmail.com>
2022-11-15 15:23:15 -0500

    Functions for random Computer Color choices created

--- main.js
+++ main.js
@@ -10,17 +10,38 @@ const quitBtn = document.querySelector(".quit")
 
 
 // create a temp dynamic array to store computer chosen sequence
-// red = box1; green = box2; blue = box3; yellow = box4
-// create function to choose box
-// if box# selected then change(blink) color accordingly
-// store box selected into temp array
+// create function to choose color
+// if color selected then change(blink) color accordingly
+// store color selected into temp array
 // take user input && if userInput === computerSequence => continue; else "GAME OVER" & break;
-// generate new box# to select, blink then append to temp array that stores computer generated sequence
-// take user input and compare to stored computer input and continue cycle
+// generate new color to select, blink then append to temp array that stores computer generated sequence
+// take user input and compare to stored computer input and continue cycle until user fails.
 
-// temp arrays to store order of button sequences
-let computerChoice = [];
+// to store player color choices
 let playerChoice = [];
+
+
+// starts game and displays first sequence sequence
+function startGame(){
+
+    //nextLvl();
+}
+
+// call this to find new color; (if user correct then push this return to computerChoice array)
+function newColor(){
+    // store possible choices
+    const colors = ["red", "green", "blue", "yellow"];
+    // select random number from array & return
+    const randColor = colors[Math.floor(Math.random() * colors.length)];
+    return randColor;
+}
+
+// begins the next level and pushes new color to the array
+function nextLvl(){
+    const computerChoice = [];
+    computerChoice.push(newColor());
+}
+
 
 function tempFunction(){
 
@@ -31,5 +52,5 @@ greenBtn.addEventListener("click", tempFunction);
 blueBtn.addEventListener("click", tempFunction);
 yellowBtn.addEventListener("click", tempFunction);
 // Menu Event Listeners
-startBtn.addEventListener("click", tempFunction);
+startBtn.addEventListener("click", startGame);
 quitBtn.addEventListener("click", tempFunction);
