4f478f118f58a869fff322863486c3a70b91106b
unknown <efraine387@gmail.com>
2022-11-16 15:43:22 -0500

    Progress on Awaiting and storing userInput

--- main.js
+++ main.js
@@ -4,17 +4,26 @@ const redBtn = document.querySelector(".red");
 const greenBtn = document.querySelector(".green");
 const blueBtn = document.querySelector(".blue");
 const yellowBtn = document.querySelector(".yellow");
+const simonBtns = document.querySelectorAll(".simon");
+console.log(simonBtns)
 // import menu buttons
 const startBtn = document.querySelector(".start");
 const quitBtn = document.querySelector(".quit");
 
-
+//////////
+// Psuedo
 // if color selected then change(blink) color accordingly
 // store color selected into temp array
 // take user input && if userInput === computerSequence => continue; else "GAME OVER" & break;
 // generate new color to select, blink then append to temp array that stores computer generated sequence
 // take user input and compare to stored computer input and continue cycle until user fails.
 
+//////////////////////////////////
+// Global Variables to keep score
+let score = 0;
+let highScore = 0;
+let playerChoice = [];
+let compColor = [];
 
 
 // call this to find new color; (if user correct then push this return to computerChoice array)
@@ -27,10 +36,22 @@ function newColor(){
 }
 
 // pushes new color to the array & starts the next level
-function nextLvl(){
-    const computerChoice = [];
+async function nextLvl(){
+    let computerChoice = [];
     computerChoice.push(newColor());
-    startLvl(computerChoice);
+    const computerInput = await startLvl(computerChoice);
+    console.log(computerInput);
+
+    // await user input
+    const userInput = await userPlay(computerChoice);
+    console.log(userInput)
+    // if user input matches computer input then begin next lvl
+    if(computerInput === userInput){
+        console.log("WE IN!")
+        score++;
+        document.querySelector(".currScore").innerHTML = `Current Score: ${score}`
+        nextLvl();
+    }
 }
 
 // starts game and displays first sequence
@@ -43,26 +64,20 @@ function quitGame(){
 }
 
 // blinks the colors passed in from startLvl
-function blinkColor(color, computerChoice){
+function blinkColor(color){
     // will call button value ".color" to blink white
     const blink = document.querySelector(`.${color}`);
     console.log(color)
     blink.style.backgroundColor = "white";
     // reverts to original color (.5sec blink duration)
     setTimeout(() => blink.style.backgroundColor = "", 180);
-    // await user input
-    userPlay(computerChoice)
-    // if user input matches computer input then begin next lvl
-    if(computerChoice === playerChoice){
-        nextLvl();
-    }
 }
 
 function userPlay(computerChoice){
     // to store player color choices
-    let playerChoice = [];
+    console.log("Waiting for player input")
     for(let i=0; i<computerChoice.length; i++){
-        playerChoice.push(readUserInput)
+        playerChoice.push(readUserInput);
     }
     return playerChoice;
 }
@@ -70,13 +85,13 @@ function userPlay(computerChoice){
 // takes the computer generated color choice array and calls blink function for each color element in array;
 function startLvl(computerChoice){
     for(let i=0; i<computerChoice.length; i++){
-        color = computerChoice[i]; // store computer selected colors to blink
-        setTimeout(blinkColor(color, computerChoice), 1000);// waits 1 second before blinking
+        setTimeout(blinkColor(computerChoice[i]), 1000);// waits 1 second before blinking
     }
+    return computerChoice;
 }
 
 
-function readUserInput(userInput){
+function readUserInput(){
     // Simons Colored Button Listeners
     redBtn.addEventListener("click", () =>{
         return `${redBtn.className}`;
